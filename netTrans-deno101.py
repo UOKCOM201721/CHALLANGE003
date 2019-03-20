c148fa8e8f0da376323e2f975cdb57dd882ea436
deno101 <dennizwarui@gmail.com>
2019-03-20 12:17:57 +0300

    implement multi servers on each valid ip

--- netTrans-deno101.py
+++ netTrans-deno101.py
@@ -1,7 +1,8 @@
 import socket
 import platform
 import subprocess as sub
-
+import threading as th
+import colorama as colo
 
 class Server:
     def __init__(self):
@@ -10,10 +11,13 @@ class Server:
     def getlocalip(self):
         if platform.system() == 'Windows':
             command = 'ipconfig | findstr IPv4'
-            print(self.fromsub(command, '\r\n'))
+            x = self.fromsub(command, '\r\n')
+            x.append('127.0.0.1')
+            return x
         elif platform.system() == 'Linux':
             command = 'ifconfig | grep netmask'
-            print(self.fromsub(command, '\n'))
+            x = self.fromsub(command, '\n')
+            return x
 
     def fromsub(self, command, nxtline):
         run = sub.Popen(command, stdout=sub.PIPE, shell=True)
@@ -30,12 +34,36 @@ class Server:
                     i -= 1
                 i += 1
             if platform.system() == 'Linux':
-            	finarray.append(nospace[1])
+                finarray.append(nospace[1])
             elif platform.system() == 'Windows':
-            	finarray.append(nospace[len(nospace)-1])
+                finarray.append(nospace[len(nospace)-1])
 
         return finarray
 
+    def main(self):
+        self.startServer()
 
-Server().getlocalip()
+    def bind(self, addr):
+        sock = socket.socket()
+        addresses = (addr, 40000)
+        sock.bind(addresses)
+        print('[' + colo.Fore.GREEN + " OK " + colo.Style.RESET_ALL + ']' + ('Success bind to {%s}' % addr))
+        while True:
+            sock.listen(10)
+            c, addrinfo = sock.accept()
+            print('[' + colo.Fore.GREEN + " CONNECT " + colo.Style.RESET_ALL + ']' + ('To {%s} ' % addrinfo[0]))
+            self.serverProcessing(c, addrinfo)
+
+    def startServer(self):
+        bindaddrs = self.getlocalip()
+        for x in bindaddrs:
+            t = th.Thread(target=self.bind, args=(x,))
+            t.start()
+
+    def serverProcessing(self, c, addrinfo):
+        pass
+
+
+if __name__ == '__main__':
+    Server().main()
 
