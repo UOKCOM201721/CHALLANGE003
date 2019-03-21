bc038601c7375fda8d878d336f3ed84458d0a898
deno101 <dennizwarui@gmail.com>
2019-03-21 23:09:37 +0300

    implement client send and server recieve

--- netTrans-deno101.py
+++ netTrans-deno101.py
@@ -3,10 +3,74 @@ import platform
 import subprocess as sub
 import threading as th
 import colorama as colo
+import struct
+import sys
+import os
+import math
+import time
+
+
+def progressbar(count, total, suffix=''):
+    barlen = 40
+    if count >= total:
+        total = count
+
+    filledlen = int(round(barlen * count/float(total)))
+    percents = round(100.0 * count / float(total), 1)
+    bar = bytes((219,)).decode('cp437') * filledlen + '-' * (barlen - filledlen)
+    sys.stdout.write("[%s] %s%s ...%s\r" % (bar, percents, "%", suffix))
+    sys.stdout.flush()
+
+
+class Client:
+    def main(self):
+        print(colo.Fore.GREEN + "Client" + colo.Style.RESET_ALL)
+        ip = input("Enter destination ip:" )
+        if ip in ('q', 'quit', 'exit'):
+            sys.exit(0)
+        sock = socket.socket()
+        addrs = (ip, 40000)
+        try:
+            sock.connect(addrs)
+        except socket.gaierror:
+            print('[Errno -2] Name or service not known')
+            self.main()
+        except ConnectionRefusedError:
+            print('[Errno 111] Connection refused')
+            self.main()
+        print('[' + colo.Fore.GREEN + "Connect" + colo.Style.RESET_ALL+ ']'+ ip)
+        filetosend = input("Enter file to send: ")
+        f = None
+        try:
+            f = open(filetosend, 'rb')
+        except FileNotFoundError:
+            print('[Errno 2] No such file or directory: ')
+            self.main()
+
+        leny = math.ceil(os.stat(filetosend).st_size)
+        filelen = struct.pack("<I", leny)
+        filenamelen = struct.pack("<I", len(filetosend))
+        sock.sendall(filelen + filenamelen)
+        sock.sendall(bytes(filetosend, 'utf-8'))
+        print('debug', len(filetosend))
+        print('debug2', len(filetosend), type(filetosend))
+        bufferx = 1024*1024
+        i = 0
+        while True:
+            data = f.read(bufferx)
+            sock.send(data)
+            progressbar(i, leny)
+            if not data:
+                break
+            i += bufferx
+        print()
+        print('[' + colo.Fore.GREEN + " SUCCESS " + colo.Style.RESET_ALL + ']' + 'sent {%s}' % filetosend)
+        self.main()
+
 
 class Server:
     def __init__(self):
-        pass
+        self.enc = 'utf-8'
 
     def getlocalip(self):
         if platform.system() == 'Windows':
@@ -51,7 +115,7 @@ class Server:
         while True:
             sock.listen(10)
             c, addrinfo = sock.accept()
-            print('[' + colo.Fore.GREEN + " CONNECT " + colo.Style.RESET_ALL + ']' + ('To {%s} ' % addrinfo[0]))
+            #print('[' + colo.Fore.GREEN + " CONNECT " + colo.Style.RESET_ALL + ']' + ('To {%s} ' % addrinfo[0]))
             self.serverProcessing(c, addrinfo)
 
     def startServer(self):
@@ -61,9 +125,32 @@ class Server:
             t.start()
 
     def serverProcessing(self, c, addrinfo):
-        pass
+        info = c.recv(8)
+        filelen = struct.unpack("<I", info[:4])[0]
+        filenamelen = struct.unpack("<I", info[4:])[0]
+        filename = c.recv(filenamelen)
+        try:
+            file = open(filename.decode('utf-8'), "wb")
+        except FileNotFoundError:
+            return
+        alldata = bytes('', 'utf-8')
+        i = 0
+        while i < filelen:
+            bufferx = 1024*1024
+            data = c.recv(bufferx)
+            file.write(data)
+            file.flush()
+            i += bufferx
+        #print()
+        #print('[' + colo.Fore.GREEN + " OK " + colo.Style.RESET_ALL + ']' + 'complete')
+        #Client().main()
 
 
 if __name__ == '__main__':
-    Server().main()
+    t = th.Thread(target=Server().main)
+    t.daemon = True
+    t.start()
+    time.sleep(1)
+    Client().main()
+
 
